46495124506ae896156b507a366d60aeed9b6420
John Nolette <john@transparent.com>
2017-07-17 22:36:43 -0400

    Imported main style rules

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ require('skeleton-css/css/normalize.css')
 require('skeleton-css/css/skeleton.css')
 require('font-awesome/font-awesome.min.css')
 
+require('@/src/styles/main.scss')
+
 require('@/app,js')
