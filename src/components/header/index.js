51e9b12236df09ccc675cda6792b549e1a470f90
John Nolette <JNolette@Transparent.com>
2017-07-22 02:39:49 -0400

    Resolved changes to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,9 +3,5 @@ import controller from '@/components/header/controller.js'
 export default {
   template: require('angularjs-template-loader!@/components/header/component.html'),
   controllerAs: 'headerController',
-<<<<<<< HEAD
   controller: controller
-=======
-  controller
->>>>>>> f1b0e623fd1e0d6249ec9907b9c4b61ef15373ab
 }
