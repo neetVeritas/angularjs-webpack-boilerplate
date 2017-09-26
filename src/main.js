edae3042836a2733024b42a8bf7bf536df949efb
John Nolette <john@neetgroup.net>
2017-09-26 01:08:34 -0400

    Fixed color shchemes, added social colors, included animate.css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 require('skeleton-css/css/normalize.css')
 require('skeleton-css/css/skeleton.css')
 require('font-awesome/css/font-awesome.min.css')
+require('animate.css')
 
 require('@/assets/styles/main.scss')
 
