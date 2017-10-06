0b1e9a5eecf5ff9ed142bb6635cb1b638c1b279b
John Nolette <john@neetgroup.net>
2017-10-05 21:45:23 -0400

    Added custom animations

--- src/components/header/controller.js
+++ src/components/header/controller.js
@@ -1,3 +1,16 @@
 export default ['$scope', function($scope) {
-
+  $scope.socialLinks = [
+    {
+      name: 'twitter',
+      href: 'https://twitter.com/neet_jn'
+    },
+    {
+      name: 'github',
+      href: 'http://github.com/neetjn'
+    },
+    {
+      name: 'linkedin',
+      href: 'https://www.linkedin.com/in/john-nolette-69ba72132'
+    }
+  ]
 }]
